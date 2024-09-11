2e2cdc9e4fc7c2ceafd953c7f8289e81c7ddc7c4
btom7447 <tombenjamin7447@gmail.com>
2024-09-11 12:17:54 +0100

    changes made to routing

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+  },
+  {
+    languageOptions: { globals: globals.browser },
+  },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    // Add this rules section
+    rules: {
+      'no-unused-vars': 'warn',
+    },
+  },
+];
